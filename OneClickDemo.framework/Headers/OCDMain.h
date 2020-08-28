8ce83992220ed373916c77308c3c46c7e60eef4a
Jed Gyeong <jed@sendbird.com>
2020-08-27 18:05:18 -0700

    Updated framework

---
+++ OneClickDemo.framework/Headers/OCDMain.h
@@ -0,0 +1,21 @@
+//
+//  OCDMain.h
+//  OneClickDemo
+//
+//  Created by Jed Gyeong on 8/26/20.
+//  Copyright © 2020 SendBird. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+/// Represents OneClick Demo class.
+@interface OCDMain : NSObject
+
+/// Sets up OneClick Demo SDK.
++ (void)setup;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ OneClickDemo.framework/Headers/OneClickDemo.h
@@ -0,0 +1,19 @@
+//
+//  OneClickDemo.h
+//  OneClickDemo
+//
+//  Created by Jed Gyeong on 8/26/20.
+//  Copyright © 2020 SendBird. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OneClickDemo.
+FOUNDATION_EXPORT double OneClickDemoVersionNumber;
+
+//! Project version string for OneClickDemo.
+FOUNDATION_EXPORT const unsigned char OneClickDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OneClickDemo/PublicHeader.h>
+
+#import "OCDMain.h"
